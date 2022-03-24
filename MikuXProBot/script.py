e3e703adfa50e2abe49e3c8f68292f0138b49c7a
Harsha <95213910+Devilharsha@users.noreply.github.com>
2022-03-24 21:50:39 +0530

    Update script.py

--- MikuXProBot/script.py
+++ MikuXProBot/script.py
@@ -1,24 +1,24 @@
-PM_START_TEXT = """────「 [{}]({}) 」────\n*Hello {}!*\n*I'm An Anime Themed Advance Group Management Bot*\n\n❍ *Uptime:* `{}`\n❍ `{}` *Users, Across* `{}` *Chats*\n\n*Click The Help Button To Get List Of My Commands.*\nThanks To @MikuXProBot For Repo"""
+PM_START_TEXT = """────「 [{}]({}) 」────\n*Hello {}!*\n*I'm An Anime Themed Advance Group Management Bot*\n\n❍ *Uptime:* `{}`\n❍ `{}` *Users, Across* `{}` *Chats*\n\n*Click The Help Button To Get List Of My Commands.*\nadd me to ur group 💓❤️💕"""
 
 MIKU_IMG = (
-      "https://telegra.ph/file/3f06de01df5bc3c3cf343.jpg",
-      "https://telegra.ph//file/05311caed13176f7d35e5.jpg",
-      "https://telegra.ph//file/88976abda0d0af9d4a517.jpg",
+      "https://telegra.ph//file/637f29bf6a42e824da699.jpg",
+      "https://telegra.ph//file/bf7bc48ecfbe795bc92e4.jpg",
+      "https://telegra.ph//file/d096dd0ac2ebf3b1287a5.jpg",
       "https://telegra.ph//file/b388f473ddfb9cc727bb1.jpg",
-      "https://telegra.ph//file/d950844dad2e473950470.jpg",
+      "https://telegra.ph//file/f520ec3057aa6cdc816ac.jpg",
 )
 
 TEXXT = "<b>Heya</b> {} <b>Do you Like me?</b>"
 
 MIKU_N_IMG = (
-      "https://telegra.ph/file/837c61d9c51236fea4100.jpg",
-      "https://telegra.ph/file/ee34cf0d7e4782424b777.jpg",
-      "https://telegra.ph/file/5410b02359a2cabc2776b.jpg",
-      "https://telegra.ph/file/b1fc3b2af759999bf3b35.jpg",
-      "https://telegra.ph/file/305b5b3c4527cd439b926.jpg"
+      "https://telegra.ph//file/f520ec3057aa6cdc816ac.jpg",
+      "https://telegra.ph//file/5a55229df83a94050f826.jpg",
+      "https://telegra.ph//file/d1f9c2c30de66cdc7ac67.jpg",
+      "https://telegra.ph//file/d096dd0ac2ebf3b1287a5.jpg",
+      "https://telegra.ph//file/bf7bc48ecfbe795bc92e4.jpg"
 
 )
 
-PM_PHOTO = "https://telegra.ph//file/3a88a958a88e83d85d284.jpg"
+PM_PHOTO = "https://telegra.ph//file/282e8f1e28f091a18a6f6.jpg"
 
-MIKU_DISPACHER_PIC = "https://telegra.ph//file/4ce99db6136c77248e0ec.jpg"
+MIKU_DISPACHER_PIC = "https://telegra.ph//file/5a55229df83a94050f826.jpg"
